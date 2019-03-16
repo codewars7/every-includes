206501fa9e482ebd304314b5bb14f755d4554e79
americanman <snowboardracer7@gmail.com>
2019-03-16 15:39:13 -0400

    Add second method with for loops

--- index.js
+++ index.js
@@ -9,3 +9,23 @@ const fun = (arr) => {
 }
 
 console.log(fun(["Mary", "Aarmy"]))
+
+
+//  second example with for loop
+//
+//  const fun = (arr) => {
+//  arr[0] = arr[0].toLowerCase()
+//  arr[1] = arr[1].toLowerCase()
+//   console.log(arr[0], arr[1])
+//   let check = []
+//   for(let i = 0; i < arr[1].length; i++) {
+//     for(let y = 0; y < arr[0].length; y++) {
+//       if(arr[1][i] == arr[0][y]) {
+//         check.push(1)
+//         break
+//       }
+//     }
+//   }
+//   console.log(arr[1].length, check.length, check )
+//   return arr[1].length == check.length ? true : false
+//   }
